c61960fe25a49981fee3c4feb61c9c0f2b573676
Raise-NCCR <raise1229@gmail.com>
2023-04-22 18:20:38 +0900

    remove index_num

--- process.py
+++ process.py
@@ -24,7 +24,7 @@ def process_file():
     indexs = completion_content.splitlines()
     for item in indexs:
         splited = item.split(',')
-        index.append({'index': splited[1], 'index_num': splited[0]})
+        index.append({'index': splited[1]})
 
     result = jsonify({'transcript': {'text': transcript}, 'index': index})
 
