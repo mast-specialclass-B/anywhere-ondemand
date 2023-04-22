190df8311047aa6bf5fa6a8eabd8617dba54bc51
Raise-NCCR <raise1229@gmail.com>
2023-04-22 17:03:43 +0900

    openai-api

--- process.py
+++ process.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import werkzeug
 import openai
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -11,15 +11,30 @@ def process_file():
     file = request.files["file"]
     filename = file.filename
     file.save(filename)
-    print(filename)
 
-    result = process_your_file(filename)
+    transcript = transcript_file(filename)
 
-    return jsonify(result)
+    content = "以下の文章についての目次を作成してください。また、出力は'1,○○\n2,××\n...'というような形で出力してください。" + transcript
 
-def process_your_file(filename):
-    result = {}
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": content}])
+
+    completion_content = completion.choices[0].message.content
+
+    index = []
+    indexs = completion_content.splitlines()
+    for item in indexs:
+        splited = item.split(',')
+        index.append({'index_num': splited[0], 'index': splited[1]})
+
+    result = jsonify({'transcript': {'text': transcript}, 'index': index})
+
     return result
+
+def transcript_file(filename):
+    openai.api_key = os.environ['OPENAI_API_KEY']
+    filename = open("sampleTokyo.wav", "rb")
+    transcript = openai.Audio.transcribe("whisper-1", filename)
+    return transcript['text']
 
 if __name__ == "__main__":
     app.run()
