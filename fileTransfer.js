628d4f403dee25b8381cd9b3bcc8e8291f1d944f
Raise-NCCR <raise1229@gmail.com>
2023-04-22 14:51:56 +0900

    basic-backend

--- fileTransfer.js
+++ fileTransfer.js
@@ -1,17 +1,21 @@
 async function uploadFile() {
 	const fileInput=document.getElementById("fileInput");
 	const file = fileInput.files[0];
-	const formData=new FromData();
+	const formData=new FormData();
 
 	formData.append("file", file);
 
-	const response = await fetch("http://localhost:80/upload", {
+	const response = await fetch("http://127.0.0.1:5000/upload", {
 		method: "POST",
 		body: formData,
 	});
+	// const response = await fetch("http://localhost:5000/", {
+	// 	method: "GET",
+	// });
 
 	if (response.ok) {
-		const result = await response.json();
+		const result = await response;
+		console.log("success");
 		console.log(result);
 	} else {
 		console.error("File upload failed");
