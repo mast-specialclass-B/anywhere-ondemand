4bb1776898cf358062da43960385a0a7e9049ef0
mioni <78632013+yuryr-kili@users.noreply.github.com>
2023-04-22 14:51:08 +0900

    jsonファイル形式に合わせる

--- indexTable.js
+++ indexTable.js
@@ -1,5 +1,7 @@
 var json =[     //jsonサンプルデータ
-      {
+    {"本文" : "ここに本文が入ります。最大で4000字程度なので、目次の後に表示した方が賢明でしょう。"
+    },
+    [{
         "目次番号":1 ,"目次":"王貞治"
       }
       ,
@@ -19,6 +21,7 @@ var json =[     //jsonサンプルデータ
         "目次番号":5 ,"目次":"清原和博"
       }
     ]
+    ]
 
       // table要素を生成
       var table = document.createElement('table');
@@ -28,7 +31,7 @@ var json =[     //jsonサンプルデータ
 
       // ヘッダーを作成
       var tr = document.createElement('tr');
-      for (key in json[0]) {
+      for (key in json[1][0]) {
             // th要素を生成
             var th = document.createElement('th');
             // th要素内にテキストを追加
@@ -40,15 +43,15 @@ var json =[     //jsonサンプルデータ
         table.appendChild(tr);
 
       // テーブル本体を作成
-      for (var i = 0; i < json.length; i++) {
+      for (var i = 0; i < json[1].length; i++) {
         // tr要素を生成
         var tr = document.createElement('tr');
         // th・td部分のループ
-        for (key in json[0]) {
+        for (key in json[1][0]) {
               // td要素を生成
               var td = document.createElement('td');
               // td要素内にテキストを追加
-              td.textContent = json[i][key];
+              td.textContent = json[1][i][key];
               // td要素をtr要素の子要素に追加
               tr.appendChild(td);
             }
