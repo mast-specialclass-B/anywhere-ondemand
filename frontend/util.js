9ddf8a4540850945cc2eecc4ad77a827d3118ed7
mioni <78632013+yuryr-kili@users.noreply.github.com>
2023-04-23 11:21:28 +0900

    loadCircleSwitch関数

--- frontend/util.js
+++ frontend/util.js
@@ -1,7 +1,13 @@
 export function errorRaise(errorMessage){
-    const loader = document.getElementById('loadCircle');
-    if(loader.classList.contains("done") == false){
-        loader.classList.add('done');
-    }
+    loadCircleSwitch(false);
     alert(errorMessage);
+}
+
+export function loadCircleSwitch(bool){
+    const loader = document.getElementById('loadCircle');
+    if(bool == true && loader.classList.contains("done") == true){
+        loader.classList.remove("done")
+    }else if(bool == false && loader.classList.contains("done") == false){
+        loader.classList.add("done")
+    }
 }
