40b7ac7d68cebdd2d827339c512c447cf728da1f
mioni <78632013+yuryr-kili@users.noreply.github.com>
2023-09-12 11:38:23 +0900

    余分なコードを削除

--- frontend/recording.js
+++ frontend/recording.js
@@ -1,19 +1,7 @@
 const record = document.getElementById('startRecord');
 const stop = document.getElementById('stopRecord');
-//const soundClips = document.querySelector('.sound-clips');
-//const canvas = document.querySelector('.visualizer');
-//const mainSection = document.querySelector('.main-controls');
-
-// disable stop button while not recording
 
 stop.disabled = true;
-
-// visualiser setup - create web audio api context and canvas
-
-//let audioCtx;
-//const canvasCtx = canvas.getContext("2d");
-
-//main block for doing the audio recording
 
 if (navigator.mediaDevices.getUserMedia) {
   console.log('getUserMedia supported.');
@@ -111,68 +99,3 @@ if (navigator.mediaDevices.getUserMedia) {
 } else {
    console.log('サポート外のブラウザです');
 }
-/*
-
-function visualize(stream) {
-  if(!audioCtx) {
-    audioCtx = new AudioContext();
-  }
-
-  const source = audioCtx.createMediaStreamSource(stream);
-
-  const analyser = audioCtx.createAnalyser();
-  analyser.fftSize = 2048;
-  const bufferLength = analyser.frequencyBinCount;
-  const dataArray = new Uint8Array(bufferLength);
-
-  source.connect(analyser);
-  //analyser.connect(audioCtx.destination);
-
-  draw()
-
-  function draw() {
-    const WIDTH = canvas.width
-    const HEIGHT = canvas.height;
-
-    requestAnimationFrame(draw);
-
-    analyser.getByteTimeDomainData(dataArray);
-
-    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-
-    canvasCtx.lineWidth = 2;
-    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
-
-    canvasCtx.beginPath();
-
-    let sliceWidth = WIDTH * 1.0 / bufferLength;
-    let x = 0;
-
-
-    for(let i = 0; i < bufferLength; i++) {
-
-      let v = dataArray[i] / 128.0;
-      let y = v * HEIGHT/2;
-
-      if(i === 0) {
-        canvasCtx.moveTo(x, y);
-      } else {
-        canvasCtx.lineTo(x, y);
-      }
-
-      x += sliceWidth;
-    }
-
-    canvasCtx.lineTo(canvas.width, canvas.height/2);
-    canvasCtx.stroke();
-
-  }
-}
-
-window.onresize = function() {
-  canvas.width = mainSection.offsetWidth;
-}
-*/
-
-//window.onresize();
