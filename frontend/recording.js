8665c7348e5015ebb89ff161bf2c937ee2a91fea
mioni <78632013+yuryr-kili@users.noreply.github.com>
2023-09-12 11:36:01 +0900

    録音機能搭載

--- frontend/recording.js
+++ frontend/recording.js
@@ -75,7 +75,7 @@ if (navigator.mediaDevices.getUserMedia) {
       //soundClips.appendChild(clipContainer);
 
       audio.controls = true;
-      const blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
+      const blob = new Blob(chunks, { 'type' : 'audio/webm; codecs=opus' });
       chunks = [];
       const audioURL = window.URL.createObjectURL(blob);
       audio.src = audioURL;
